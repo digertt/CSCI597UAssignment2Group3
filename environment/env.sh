af79633e6d4f45d38827ca52770dcf4aaf240d74
digertt <104587902+digertt@users.noreply.github.com>
2022-05-15 19:05:23 -0700

    Initial file upload

---
+++ environment/env.sh
@@ -0,0 +1,5 @@
+export AWS_REGION=us-west-1
+export USER_POOL_ID=us-west-1_6BAJcsHDe
+export COGNITO_CLIENT_ID=4r721upmp2d2181ihnfsqpj74v
+export FUNCTION_ARN=arn:aws:lambda:us-west-1:879163561573:function:turn-based-api
+export BASE_URL=https://otrj9bo4x0.execute-api.us-west-1.amazonaws.com/prod
