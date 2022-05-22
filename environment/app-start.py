7b65b5145a2ca57f42c5a37896d8788acc121734
digertt <104587902+digertt@users.noreply.github.com>
2022-05-22 00:29:53 -0700

    Most of the CLI

--- environment/app-start.py
+++ environment/app-start.py
@@ -1,3 +1,4 @@
+from multiprocessing import get_all_start_methods
 import requests
 import json
 
@@ -13,7 +14,6 @@ def main():
     while startLoop == False :
         print("Welcome! Please enter \"1\" to sign in or \"2\" to sign up!")
         init_select = input(">")
-        curUser = NULL
         if init_select == "1":
             #login
             startLoop = True
@@ -57,8 +57,34 @@ def main():
             print("Enter game id: ")
             gameId = input(">")
             curGame = requests.get('some url/games', data = payload)
-        
-        #do perform move stuff (when we get the game logic up)
+    
+     
+gameLoop = False
+entryLoop = False
+print("Welcome! Here is the current game state:")
+board = requests.get('get from DB')
+print(board[0]+ " " +board[1]+ " " +board[2])
+print(board[3]+ " " +board[4]+ " " +board[5])
+print(board[6]+ " " +board[7]+ " " +board[8])
+
+moveColumn = 1
+moveRow = 1
+
+while entryLoop == False:
+
+    print("Please enter the column you want to move in: ")
+    moveColumn = input(">")
+    print("Please enter the row you want to move in: ")
+    moveRow = input(">")
+    if (moveColumn > 3 or moveColumn < 1) or (moveRow > 3 or moveRow < 1):
+        print("Invalid entry. Please enter a value between 1 and 3")
+    elif board [((moveRow-1)*3) + (moveColumn-1)] != "0":
+        print("Cannot move there! Please select a valid location!")
+    else:
+        entryLoop = True
+
+print("Sending move...")
+send = requests.get() #send move data, will figure out format soon
 
 if __name__ == "__main__":
     main()
