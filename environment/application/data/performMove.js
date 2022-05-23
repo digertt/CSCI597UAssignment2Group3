c276caf19b93e014fd872549fb58ded949c8922f
digertt <104587902+digertt@users.noreply.github.com>
2022-05-22 23:46:26 -0700

    Game logic

--- environment/application/data/performMove.js
+++ environment/application/data/performMove.js
@@ -3,22 +3,28 @@
 const AWS = require("aws-sdk");
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
-const performMove = async ({ gameId, user, changedHeap, changedHeapValue }) => {
-  if (changedHeapValue < 0) {
-    throw new Error("Cannot set heap value below 0");
+const performMove = async ({ gameId, user, board, coords }) => {
+    if (coords > 9 || coords < 1) {
+    throw new Error('Cannot set heap value below 0')
   }
+  coords = coords - 1
+  updatemark = 'X'
+  if ('user1 = :user')
+    updatemark = 'X'
+  else
+    updatemark = 'O'
   const params = {
-    TableName: "turn-based-game",
-    Key: {
+    TableName: 'turn-based-game',
+    Key: { 
       gameId: gameId
     },
-    UpdateExpression: `SET lastMoveBy = :user, ${changedHeap} = :changedHeapValue`,
-    ConditionExpression: `(user1 = :user OR user2 = :user) AND lastMoveBy <> :user AND ${changedHeap} > :changedHeapValue`,
+    UpdateExpression: `SET lastMoveBy = :user, ${board[coords]} = :updatemark`,
+    ConditionExpression: `(user1 = :user OR user2 = :user) AND lastMoveBy <> :user AND ${board[coords]} == ' '`,
     ExpressionAttributeValues: {
-      ":user": user,
-      ":changedHeapValue": changedHeapValue
+      ':user': user,
+      ':coords': coords
     },
-    ReturnValues: "ALL_NEW"
+    ReturnValues: 'ALL_NEW'
   };
   try {
     const resp = await documentClient.update(params).promise();
