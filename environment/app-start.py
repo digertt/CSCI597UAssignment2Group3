7451153cc747a91e95bc5bff5db49a1f639e6e9c
brzosks <67334912+brzosks@users.noreply.github.com>
2022-05-24 23:33:41 -0700

    Add files via upload
    updated and added LOTS of features to app-start.py

--- environment/app-start.py
+++ environment/app-start.py
@@ -2,96 +2,166 @@ from multiprocessing import get_all_start_methods
 import requests
 import json
 
+
+BASE_URL = "https://q9ziogix3f.execute-api.us-west-1.amazonaws.com/prod" #server url
+
 def main():
-    #curUser = null
+    usrName = ""
     #curGame = null
     startLoop = False
-    apiToken = "" 
-    resp = requests.get("some-url")
-    if resp != 200:
-        print ("error connecting to service")
-        return
+    apiToken = ""
     curUserData = ""
+    idToken = ""
     while startLoop == False :
         print("Welcome! Please enter \"1\" to sign in or \"2\" to sign up!")
         init_select = input(">")
         if init_select == "1":
             #login
-            startLoop = True
-            print("please enter your email: ")
-            usrEmail = input(">")
-            print("please enter your password: ")
+            print("Please enter your username: ")
+            usrName = input(">")
+            print("Please enter your password: ")
             usrPassword = input(">")
-            usrDat = {"email":" usrEmail", "password":"usrPassword"}
+            usrDat = {"username": usrName, "password": usrPassword}
             usrDatJson = json.dumps(usrDat)
-            payload = {"json_payload":usrDatJson}
-            curUser = requests.post('some url/users', data = payload)
-            curUserData = json.load(curUser.text)
+            curUser = requests.post(BASE_URL + '/login', data = usrDatJson, headers={'Content-Type': 'application/json'})
+            curUserData = json.loads(curUser.text)
+            if "message" in curUserData: #error was returned
+                print("Server returned error:")
+                print(curUserData["message"])
+            else:
+                startLoop = True
+                idToken = curUserData['idToken']
             
         elif init_select == "2":
             #sign up
-            startLoop = True
-            print("please enter your new email: ")
+            print("Please enter your username: ")
+            usrName = input(">")
+            print("Please enter your new email: ")
             usrEmail = input(">")
-            print("please enter your new password: ")
+            print("Please enter your new password: ")
             usrPassword = input(">")
-            usrDat = {"email":" usrEmail", "password":"usrPassword"}
+            usrDat = {'username':usrName, 'password':usrPassword, 'email':usrEmail}
             usrDatJson = json.dumps(usrDat)
-            payload = {"json_payload":usrDatJson}
-            curUser = requests.post('some url/login', data = payload)
-            curUserData = json.load(curUser.text)
+            curUser = requests.post(BASE_URL + '/users', data = usrDatJson)
+            curUserData = json.loads(curUser.text)
+            if "message" in curUserData: #error was returned
+                print("Server returned error:")
+                print(curUserData["message"])
+            else:
+                print("Please verify email then login")
         else:
             print("Please enter a valid selection")
     menuLoop = False
+    gameId = None
+    curGameData = ""
     while menuLoop == False:
         
-        print("Enter 1 to start a new game, or 2 to access an existing game")
+        print("***Main Menu***")
+        print("Enter 1 to start a new game\nEnter 2 to access an existing game\nEnter 3 to access saved game\nEnter 4 to exit program")
         menu_select = input(">")
         if menu_select == "1":
-            menuLoop == True
             print("Enter opponent: ")
             opponent = input(">")
             gameDat = {"opponent" : opponent}
-            usrDatJson = json.dumps(usrDat)
-            payload = {"json_payload":usrDatJson, 'apikey': "Do we need this?"}
-            curGame = requests.post('some url/games', data = payload, header = {"authorization" : curUserData["idToken"]})
-            curGameData = json.load(curGame.text)
+            usrDatJson = json.dumps(gameDat)
+            curGame = requests.post(BASE_URL + '/games', data = usrDatJson, headers = {'Content-Type': 'application/json', "Authorization" : curUserData["idToken"]})
+            curGameData = json.loads(curGame.text)
+            if "message" in curGameData: #error was returned
+                if curGameData["message"] == "Cannot read property 'Username' of undefined":
+                    print("User was not found :(\n")
+                else:
+                    print("Server returned error:")
+                    print(curGameData["message"])
+            else:
+                print("Challenge Sent!")
+                gameId = curGameData["gameId"]
             
-        elif menu_select == 2:
-            menuLoop == True
+        elif menu_select == "2":
             print("Enter game id: ")
             gameId = input(">")
-            curGame = requests.post('some url/games/' + gameId, header = {"authorization" : curUserData["idToken"]})
-            curGameData = json.load(curGame.text)
+            curGameData = requests.get(BASE_URL + '/games/' + gameId, headers = {'Content-Type': 'application/json', "Authorization" : curUserData["idToken"]})
+            if curGameData.text == "":
+                print("Invalid game Id\n")
+            else:
+                print(curGameData.text)
+                curGameData = json.loads(curGameData.text)
+                # need to handle case where the game has been deleted/ there is no game state.
+                if "message" in curGameData:
+                    print("That game ID does not exist\n")
+                else:
+                    performMove(curGameData, curUserData, gameId, usrName)
+
+        elif menu_select == "3":
+            print("Checking last saved game...")
+            if gameId != None:
+                curGameData = requests.get(BASE_URL + '/games/' + gameId, headers = {'Content-Type': 'application/json', "Authorization" : curUserData["idToken"]})
+                if curGameData.text == "":
+                    print("Invalid game Id\n")
+                else:
+                    curGameData = json.loads(curGameData.text)
+                    performMove(curGameData, curUserData, gameId, usrName)
+            else:
+                print("No saved game :(\n")
+
+        elif menu_select == "4":
+            exit()
         else:
-            print("Please enter a valid selection")
-     
-gameLoop = False
-entryLoop = False
-print("Welcome! Here is the current game state:")
-board = requests.get('get from DB')
-print(board[0]+ " " +board[1]+ " " +board[2])
-print(board[3]+ " " +board[4]+ " " +board[5])
-print(board[6]+ " " +board[7]+ " " +board[8])
+            print("Please enter a valid selection\n")
 
-moveColumn = 1
-moveRow = 1
+def performMove(curGameData, curUserData, gameId, usrName):
+    gameLoop = False
+    entryLoop = False
 
-while entryLoop == False:
-
-    print("Please enter the column you want to move in: ")
-    moveColumn = input(">")
-    print("Please enter the row you want to move in: ")
-    moveRow = input(">")
-    if (moveColumn > 3 or moveColumn < 1) or (moveRow > 3 or moveRow < 1):
-        print("Invalid entry. Please enter a value between 1 and 3")
-    elif board [((moveRow-1)*3) + (moveColumn-1)] != "0":
-        print("Cannot move there! Please select a valid location!")
+    print("Welcome! Here is the current game state:")
+    board = curGameData["board"]
+    
+    printBoard(board)
+    print("The last user to move was: " + str(curGameData["lastMoveBy"]))
+    if curGameData["lastMoveBy"] == usrName:
+        print("You made the last move...")
+        print("Returning to menu\n")
+        return
+    
+    moveColumn = 1
+    moveRow = 1
+    moveSpot = 1
+    
+    while entryLoop == False:
+    
+        print("Please enter the column you want to move in: ")
+        moveColumn = input(">")
+        print("Please enter the row you want to move in: ")
+        moveRow = input(">")
+        if (moveColumn != "1" and moveColumn != "2" and moveColumn != "3") and (moveRow != "1" and moveRow != "2" and moveRow != "3"):
+            print("Invalid entry. Please enter a value between 1 and 3")
+        else:
+            entryLoop = True
+    
+    moveColumn = int(moveColumn)
+    moveRow = int(moveRow) - 1
+    moveSpot = (moveRow*3) + moveColumn
+    print("Sending move...")
+    moveDat = {"coords": moveSpot}
+    moveDatJson = json.dumps(moveDat)
+    send = requests.post(BASE_URL + "/games/" + gameId, data = moveDatJson, headers = {'Content-Type': 'application/json',"Authorization" : curUserData["idToken"]}) #send move data, will figure out format soon
+    verifyMove = json.loads(send.text)
+    if "message" in verifyMove: #error was returned
+        print("Server returned error:")
+        print(verifyMove["message"])
     else:
-        entryLoop = True
+        print("Move sent!")
 
-print("Sending move...")
-send = requests.get() #send move data, will figure out format soon
+def printBoard(board):
+    print("   |   |   ")
+    print(" " + board[0] + " | " + board[1] + " | " + board[2])
+    print("___|___|___")
+    print("   |   |   ")
+    print(" " + board[3] + " | " + board[4] + " | " + board[5])
+    print("___|___|___")
+    print("   |   |   ")
+    print(" " + board[6] + " | " + board[7] + " | " + board[8])
+    print("   |   |   ")
+
 
 if __name__ == "__main__":
     main()
