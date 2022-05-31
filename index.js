b6e51db0cc064bb2cfbae6d92cf3c97e9766b2bd
digertt <104587902+digertt@users.noreply.github.com>
2022-05-31 00:08:53 -0700

    Create index.js
    Start of lambda function, may need to change this based on how we format sqs messages

---
+++ index.js
@@ -0,0 +1,28 @@
+const AWS = require('aws-sdk');
+const DDB = new AWS.DynamoDB.DocumentClient({
+    version: '2012-08-10',
+    region: 'us-west-1'
+});
+
+exports.handler = async (event) => {
+    try{
+        const {message} = event;
+        const curBody = JSON.parse(message.body); //we may need to get multiple items
+        
+        const curPost = {
+            TableName: 'our-table-name',
+            Item:{
+                username: curBody.username,
+                title: curBody.title,
+                content: curBody.content
+            }
+        };
+        await DDB.put(curPost).promise();
+    }
+    
+    catch(error){
+        console.log(error);
+        return;
+    }
+    
+};
